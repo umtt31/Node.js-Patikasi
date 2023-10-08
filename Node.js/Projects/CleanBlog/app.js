cfbe8ddc01ffd2a91cadd1ce488268b771c7ee40
umtt31 <umutcan.adiguzel@outlook.com>
2023-10-09 01:44:57 +0300

    odev10a kadar yapildi

--- Node.js/Projects/CleanBlog/app.js
+++ Node.js/Projects/CleanBlog/app.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+mongoose.connect('mongodb://localhost:27017/clean-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', async (req, res) => {
+  let posts = await Post.find({});
+  res.render('index', { posts });
+});
+
+app.get('/post/:id', async (req, res) => {
+  let post = await Post.findById(req.params.id);
+  res.render('post', { post });
+});
+
+app.get('/addpost', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/addpost', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3002;
--- Node.js/Projects/PCAT/app.js
+++ Node.js/Projects/PCAT/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.set(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get('/', (req, res) => {
   console.log('a Request');
